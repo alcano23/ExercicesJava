1806e8e5e41e885fd496676850b78accd299b437
Didier SEWAGNON <didier.sewagnon@ism.edu.sn>
2021-03-26 12:35:23 +0100

    exercice4

---
+++ Exo4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exo4{
+    public static void main(String[] args) {
+        System.out.print("Donner un jour:");
+        Scanner clavier=new Scanner(System.in);
+        int j = clavier.nextInt();
+        System.out.print("Donner un mois:");
+        Scanner clavier2=new Scanner(System.in);
+        int m = clavier2.nextInt();
+        System.out.print("Donner une année:");
+        Scanner clavier3=new Scanner(System.in);
+        int a = clavier3.nextInt();
+   } 
+}
